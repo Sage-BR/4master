0c605c0317f9234c798ab50310715d5c3be94d9d
Saulo Germano <sgwebr@gmail.com>
2024-12-23 22:03:37 -0300

    Add files via upload

---
+++ mariadb.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Função para exibir mensagens coloridas
+function echo_info {
+    echo -e "\e[34m[INFO]\e[0m $1"
+}
+
+function echo_success {
+    echo -e "\e[32m[SUCCESS]\e[0m $1"
+}
+
+function echo_error {
+    echo -e "\e[31m[ERROR]\e[0m $1"
+}
+
+# Verifica se o script está sendo executado como root
+if [ "$EUID" -ne 0 ]; then
+    echo_error "Este script deve ser executado como root."
+    exit 1
+fi
+
+# Solicita informações ao usuário
+read -p "Digite o nome do usuário administrador do MariaDB: " mariadb_user
+read -sp "Digite a senha para o usuário $mariadb_user: " mariadb_password
+echo
+read -p "Digite a porta que o MariaDB deve usar [default: 3306]: " mariadb_port
+mariadb_port=${mariadb_port:-3306}
+read -p "Permitir conexões remotas? (y/n) [default: n]: " allow_remote
+allow_remote=${allow_remote:-n}
+
+# Atualiza pacotes e instala o MariaDB
+echo_info "Atualizando pacotes e instalando o MariaDB..."
+apt update && apt install -y mariadb-server mariadb-client
+if [ $? -ne 0 ]; then
+    echo_error "Falha ao instalar o MariaDB."
+    exit 1
+fi
+
+# Configura o MariaDB
+echo_info "Configurando o MariaDB..."
+systemctl start mariadb
+systemctl enable mariadb
+
+# Configura o arquivo de configuração do MariaDB
+cat <<EOF > /etc/mysql/mariadb.conf.d/99-custom.cnf
+[mysqld]
+bind-address = 0.0.0.0
+port = $mariadb_port
+EOF
+
+if [ "$allow_remote" == "y" ]; then
+    sed -i 's/^bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+fi
+
+# Reinicia o MariaDB para aplicar as configurações
+echo_info "Reiniciando o MariaDB..."
+systemctl restart mariadb
+
+# Configura o usuário administrador e permissões
+echo_info "Configurando usuário e permissões..."
+mariadb -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$mariadb_password');"
+mariadb -e "CREATE USER IF NOT EXISTS '$mariadb_user'@'%' IDENTIFIED BY '$mariadb_password';"
+mariadb -e "GRANT ALL PRIVILEGES ON *.* TO '$mariadb_user'@'%' WITH GRANT OPTION;"
+mariadb -e "FLUSH PRIVILEGES;"
+
+if [ "$allow_remote" == "y" ]; then
+    mariadb -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$mariadb_password';"
+    mariadb -e "FLUSH PRIVILEGES;"
+fi
+
+# Confirmação final
+echo_success "Instalação e configuração do MariaDB concluídas."
+echo_info "Usuário: $mariadb_user"
+echo_info "Senha: (oculta)"
+echo_info "Porta: $mariadb_port"
+if [ "$allow_remote" == "y" ]; then
+    echo_info "Conexões remotas: Habilitadas"
+else
+    echo_info "Conexões remotas: Desabilitadas"
+fi
