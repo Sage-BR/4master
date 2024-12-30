8b039cdc412d9b62365acb318436117988472280
Saulo Germano <sgwebr@gmail.com>
2024-12-30 20:43:00 -0300

    Update mem.sh

--- mem.sh
+++ mem.sh
@@ -1,50 +1,36 @@
 #!/bin/sh
- 
-# ********************** #
-#    remontti.com.br     #
-# ********************** #
- 
-# Porcentagem máxima (mude se você achar necessário)
-percent=85
- 
-# Total da memoria:
-ramtotal=`grep -F "MemTotal:" < /proc/meminfo | awk '{print $2}'`
-# Memoria livre:
-ramlivre=`grep -F "MemFree:" < /proc/meminfo | awk '{print $2}'`
- 
-# RAM utilizada pelo sistema:
-ramusada=`expr $ramtotal - $ramlivre`
- 
-# Porcentagem de RAM utilizada pelo sistema:
-putil=`expr $ramusada \* 100 / $ramtotal`
- 
-echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+# Limite de uso de memória
+percent=70
+
+# Total e memória livre
+ramtotal=$(grep -F "MemTotal:" /proc/meminfo | awk '{print $2}')
+ramlivre=$(grep -F "MemFree:" /proc/meminfo | awk '{print $2}')
+
+# Memória utilizada e porcentagem
+ramusada=$((ramtotal - ramlivre))
+putil=$((ramusada * 100 / ramtotal))
+
+# Log do estado atual
+echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
 date
-echo
-echo "Mem. utilizada: $putil %";
- 
-if [ $putil -gt $percent ]
-then
-date=`date`
-echo $date >> /var/log/memoria.log
-echo "Mem. utilizada: $putil %" >> /var/log/memoria.log
- 
-echo "Memoria acima de $percent %, cache foi limpado!";
-sync
-# 'Dropando' cache:
-echo 3 > /proc/sys/vm/drop_caches
-# Caso nao esteja utilizanod debian, descomente a linha abaixo
-# sysctl -w vm.drop_caches=3 
-# Zera SWAP
-swapoff -a
-swapon -a
-echo
-free -m
-echo
-echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+echo "Memória total: $ramtotal kB"
+echo "Memória livre: $ramlivre kB"
+echo "Memória utilizada: $putil%"
+
+if [ $putil -gt $percent ]; then
+    # Log detalhado
+    logfile="/var/log/memoria.log"
+    [ ! -f $logfile ] && touch $logfile
+    date >> $logfile
+    echo "Memória acima de $percent%: $putil%" >> $logfile
+
+    echo "Limpando cache e reiniciando SWAP..."
+    sync
+    echo 3 > /proc/sys/vm/drop_caches
+    swapoff -a && swapon -a
 else
-echo "Cache não foi limpado!";
-echo =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-unset percent ramtotal ramlivre ramusada putil
-exit $?
+    echo "Uso de memória abaixo do limite. Nenhuma ação necessária."
 fi
+
+echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
