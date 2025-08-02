b86be7d0a3d1ffc2ee2d59489b5967dd51ffe214
Saulo Germano <sgwebr@gmail.com>
2025-08-02 11:01:26 -0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+set -e
+
+echo "Iniciando atualização em $(date)"
+
 apt update
 apt -y dist-upgrade
 apt -y autoremove
 apt clean
-apt purge -y $(dpkg -l | awk '/^rc/ { print $2 }')
+
+# Remove pacotes com config residual (rc)
+RESIDUAL_PKGS=$(dpkg -l | awk '/^rc/ { print $2 }')
+if [ -n "$RESIDUAL_PKGS" ]; then
+  apt purge -y $RESIDUAL_PKGS
+fi
+
+echo "Atualização concluída em $(date)"
